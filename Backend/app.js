7cc5c83ec291e1054b69d891314b99c141b6d24c
Oscar Wong <wong.oscar67@gmail.com>
2019-11-19 13:03:37 -0700

    initial commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+let Koa = require('Koa')
+let cors = require('koa-cors')
+let bodyparser = require('koa-bodyparser')
+var path = require('path')
+let autoRoutes = require('koa-auto-routes')
+
+let app = new Koa()
+
+app
+    .use(cors())
+    .use(bodyparser())
+
+autoRoutes(app, path.join(__dirname, 'routers'))
+
+app.listen(5000)
+console.log('Server running on port 5000')
---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'remotemysql.com',
+  user     : 'cfxpQFeAgl',
+  password : '5wLicDMtkr',
+  database : 'cfxpQFeAgl'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
