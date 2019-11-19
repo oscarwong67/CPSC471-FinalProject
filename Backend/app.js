de75f48700dad128b66343b0d5ef555f104aaa90
Oscar Wong <wong.oscar67@gmail.com>
2019-11-19 13:19:30 -0700

    add more docs

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,23 @@ let cors = require('koa-cors')
 let bodyparser = require('koa-bodyparser')
 var path = require('path')
 let autoRoutes = require('koa-auto-routes')
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'remotemysql.com',
+    user: 'cfxpQFeAgl',
+    password: '5wLicDMtkr',
+    database: 'cfxpQFeAgl'
+});
+
+connection.connect();
 
 let app = new Koa()
+
+connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    console.log('If this prints "2", then the database is online and working. ', results[0].solution);
+});
 
 app
     .use(cors())
--- Backend/initDb.js
+++ Backend/initDb.js
@@ -8,9 +8,11 @@ var connection = mysql.createConnection({
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
+// connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// });
+
+
  
 connection.end();
