8b7c873711b0aad6e05075dcfce7ef5deff7f08c
Christina <christinalu42@gmail.com>
2019-11-27 14:18:45 -0700

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
 import { Header } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 function App() {
+
   return (
     <div className="App">
       <Header as='h1' color='pink' className="App-header">
         Welcome to Ryde
       </Header>
+
+      <Button 
+      onClick = {console.log("clicked")} >
+       Login
+       </Button>
+      
+      
+
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
+
+class Login extends Component{
+  render = () => (
+    <Segment placeholder>
+      <Grid columns={2} relaxed='very' stackable>
+        <Grid.Column>
+          <Form>
+            <Form.Input
+              icon='at'
+              iconPosition='left'
+              label='Email'
+              placeholder='Email'
+              onChange= {this.handleEmailChange}
+            />
+            <Form.Input
+              icon='lock'
+              iconPosition='left'
+              label='Password'
+              type='password'
+            />
+
+            <Button content='Login' primary />
+          </Form>
+        </Grid.Column>
+
+        <Grid.Column verticalAlign='middle'>
+          <Button content='Sign up' icon='signup' size='big' />
+        </Grid.Column>
+      </Grid>
+
+      <Divider vertical>Or</Divider>
+    </Segment>
+  )
+
+  handleEmailChange(event){
+    //set state
+  }
+}
+
+
+
+export default DividerExampleVerticalForm
