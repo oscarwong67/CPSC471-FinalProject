83d681eefcd687b01bb60b9a70d111c09b68b22c
Christina <christinalu42@gmail.com>
2019-11-27 18:41:10 -0700

    Update ChargerDashboard.js

--- frontend/src/Components/Employee/Charger/ChargerDashboard.js
+++ frontend/src/Components/Employee/Charger/ChargerDashboard.js
@@ -4,21 +4,45 @@ import { Button } from 'semantic-ui-react'
 import Dashboard from '../Dashboard'
 
 class ChargerDashboard extends React.Component{
+  constructor(props){
+    super (props);
+    percentage: ''
+    vehicleId: ''
+  }
 
   render = () => (
     <div>
       <Dashboard/>
-      <Input focus placeholder='Enter Vehicle ID' />
+      <Input focus placeholder='Enter Vehicle ID'
+      onUpdate = {this.handleUpdateVehicleId} />
       <Input
         label={{ basic: true, content: '%' }}
         labelPosition='right'
         placeholder='Enter Percentage Charged'
+        onUpdate = {this.handleUpdatePercentage}
       />
-      <Button>
+      <Button
+      onClick = { this.submitCharge }>
         Enter
       </Button>
     </div>
     
   );
+
+  submitCharge = () => {
+    //todo
+  }
+
+  handleUpdateVehicleId = (event) => {
+    this.setState({
+      vehicleId: event.target.value
+    })
+  }
+
+  handleUpdateVehicleId = (event) => {
+    this.setState({
+      percentage: event.target.value
+    })
+  }
 
 }
